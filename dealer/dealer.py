ca06fbff367a69627eb3378e288657b62de2786c
Mariano Rapaport <v-mrapaport@acvauctions.com>
2022-11-20 21:16:25 -0300

    update dealer

--- dealer/dealer.py
+++ dealer/dealer.py
@@ -1,10 +1,12 @@
 from google.cloud import pubsub_v1
 import json
 from datetime import datetime, timedelta
+import threading
+import pandas as pd
 
-best_precision = -1
-best_combination = {}
 last_message_timestamp = datetime.today()
+
+output_data = {}
 
 topic_name = 'projects/{project_id}/subscriptions/{sub}'.format(
             project_id="cryptic-opus-335323",
@@ -13,32 +15,49 @@ topic_name = 'projects/{project_id}/subscriptions/{sub}'.format(
 
 subscriber = pubsub_v1.SubscriberClient()
 
+class BackgroundTasks(threading.Thread):
+    def run(self,*args,**kwargs):
+        while True:
+            if old_execution_is_done():
+                print("Old execution is done")
+                save_and_upload_results()
+                break
+            print("Execution is still going")                
+            time.sleep(30)
+
+    def save_and_upload_results():
+        pd.from_dict(output_data).to_csv("whey_results.csv")
+        storage_client = storage.Client()
+        bucket = storage_client.bucket("bdm-unlu")
+        blob = bucket.blob('whey_results.csv')
+        blob.upload_from_filename('whey_results.csv')
+
 def old_execution_is_done():
     return datetime.today() > last_message_timestamp + timedelta(minutes=3)
 
-def reset_execution():
-    global best_combination
-    global best_precision
-    print("Best parameters so far: {}".format(best_combination))
-    print("Starting new execution")
-    best_precision = -1
-    best_combination = {}
 
 def callback(message):
     global best_precision
     global last_message_timestamp
     global best_combination
-    if old_execution_is_done():
-        reset_execution()
+    global output_data
     last_message_timestamp = datetime.today()
     data = json.loads(message.data.decode("utf-8"))
-    #print(data)
     results = data["results"]
-    mae = results["mae"]
-    if mae > best_precision:
-        best_precision = mae
-        best_combination = data
-        print("New best found {}".format(data))
+
+    for key in data:
+        #if type(data[key]) is dict:
+        #    for subkey in data[key]:
+        #        if not subkey in output_data:
+        #            output_data[subkey] = [data[key][subkey]]
+        #        else:
+        #            output_data[subkey].append(data[key][subkey])
+        #else:
+        if key not in output_data:
+            output_data[key] = [data[key]]
+        else:
+            output_data[key].append(data[key])
+
     message.ack()
 
 future = subscriber.subscribe(topic_name, callback)
